d0207c4aeb05ea3e0b7ed7e504653d2fd33f54e3
BackandFront <www@backandfront.tk>
2019-07-04 16:39:22 +0100

    Initial commit

---
+++ AlkisNet/index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Alki&#351; Hotspot Internet</title>
+<style type="text/css">
+.style1 {
+text-align: center;
+width:1000px;
+background:white;
+margin-left:auto;
+margin-right:auto;
+height:510px;
+padding-left:5px;
+padding-right:5px;
+border:2px solid black;
+}
+.style4{
+border:1px rgb(204, 204, 204) solid;padding:7px;
+}
+.style5{
+font-size:16px;
+font-weight:bold;
+text-shadow:rgb(255, 255, 255) 1px 1px 1px; 
+}
+.style6{
+border-style: none;
+border-color: inherit;
+border-width: 0px;
+background: url('images/bg_input.png') no-repeat 0px -51px;
+font-size:28px;
+height:39px;
+padding-left:10px;
+padding-top: 12px;
+width: 409px;
+}
+.style7{
+font-size:11px;
+text-align:center 
+}
+.style8 {
+height:40px;
+font-size:14px;
+font-weight:bold;
+width: 420px; 
+}
+.style9 {
+color: #336699;
+text-decoration:underline;
+}
+.style10 {
+border-bottom:1px gray solid;
+}
+.style11 {
+font-weight: bold;
+font-size: 16px;
+color:maroon;
+}
+
+</style>
+<script type="text/javascript" src="/md5.js"></script>
+<script type="text/javascript">
+<!--
+function doLogin() {
+document.sendin.username.value = document.login.username.value;
+document.sendin.password.value = hexMD5('\250' + document.login.password.value + '\330\207\016\216\314\113\106\327\036\011\015\201\127\022\360\047');
+document.sendin.submit();
+return false;
+}
+//-->
+</script>
+</head>
+<body style="background-image: url('images/bg.gif');font-family:'Segoe UI';font-size:13px;color:rgb(97, 101, 109)">
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div class="style1">
+<h1 class="style9">Alk<span lang="tr">&#305;&#351;</span> Hotspot Internet</h1>
+<div class="content" style="padding-bottom: 10px; ">
+<form action="http://www.castlehotspot.com/login" method="post" name="sending" onsubmit="return doLogin()">
+<input type="hidden" name="username" />
+<input type="hidden" name="password" />
+<input type="hidden" name="dst" value="http://www.castlehotspot.com/login" />
+<input type="hidden" name="popup" value="true" />
+
+<table border="0" width="100%">
+<tr>
+<td align="center" valign="top">
+<h2 style="margin-bottom: 10px; font-size: 18px;" class="style9">Login With Your Username &amp; Password To Use Internet</h2>
+<div class="style4">
+<table border="0" width="100%">
+<tr><td align="center"><label id="lusername" class="style5">Username</label></td></tr>
+<tr><td align="center"><input id="username" class="style6" maxlength="16" name="username" value="" type="text" /></td>
+</tr>
+<tr><td align="center"><label id="lpassword" class="style5">Password</label></td></tr>
+<tr><td align="center"><input id="password" class="style6" maxlength="16" name="password" value="" type="password" /></td>
+</tr>
+<tr><td>&nbsp;</td></tr>
+<tr><td align="center"><input id="login" class="style8" type="submit" value="Login" /></td></tr>
+<tr><td class="style7">&nbsp;</td></tr>
+</table>
+</div>
+<br clear="all" />
+<div class="borderedbox" style="border-top-color: rgb(204, 204, 204); border-right-color: rgb(204, 204, 204); border-bottom-color: rgb(204, 204, 204); border-left-color: rgb(204, 204, 204); border-top-style: solid; border-right-style: solid; border-bottom-style: solid; border-left-style: solid; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; padding-top: 7px; padding-right: 7px; padding-bottom: 7px; padding-left: 7px; ">
+<table border="0" width="100%">
+<tr>
+<td style="text-align:center" class="style11">
+Can't login? Don't have a Username &amp; Password?
+<br />
+<br />
+Call this number to get yours:<strong> <span class="style9">05338540297</strong></span></td>
+</tr>
+</table>
+</div>
+
+</td>
+<td valign="top" width="2"></td>
+<td align="center" valign="top">
+<h2 style="margin-bottom:10px;font-size:18px;" class="style9">Internet Packages &amp; Prices</h2>
+<div class="style4">
+<table align="center" border="0" width="400px" cellpadding="3" cellspacing="0">
+<tr><td style="text-align:center" class="style10"><strong>Speed</strong></td><td style="text-align:center" class="style10"><strong>Quota</strong></td><td style="text-align:center" class="style10"><strong>Price</strong></td></tr>
+<tr><td style="text-align:center">1024 mbps</td><td style="text-align:center">1 GB</td><td style="text-align:center">10TL</td></tr>
+<tr><td style="text-align:center">1024 mbps</td><td style="text-align:center">3 GB</td><td style="text-align:center">10TL</td></tr>
+<tr><td style="text-align:center">1024 mbps</td><td style="text-align:center">5 GB</td><td style="text-align:center">10TL</td></tr>
+<tr><td style="text-align:center">1024 mbps</td><td style="text-align:center">10 GB</td><td style="text-align:center">10TL</td></tr>
+<tr><td style="text-align:center">512 mbps</td><td style="text-align:center">UNLIMITED</td><td style="text-align:center">10TL</td></tr>
+</table>
+</div>
+<h2 class="style9">Check Your Quota Usage</h2>
+<div class="borderedbox" style="border-top-color: rgb(204, 204, 204); border-right-color: rgb(204, 204, 204); border-bottom-color: rgb(204, 204, 204); border-left-color: rgb(204, 204, 204); border-top-style: solid; border-right-style: solid; border-bottom-style: solid; border-left-style: solid; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px; padding-top: 7px; padding-right: 7px; padding-bottom: 7px; padding-left: 7px; ">
+<table border="0" width="100%">
+<tr>
+<td style="font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif; font-size: 13px; color: rgb(97, 101, 109);text-align:center ">
+<label id="lusername0" style="font-size: 16px; font-weight: bold; text-shadow: rgb(255, 255, 255) 1px 1px 1px; ">
+Username</label></td>
+</tr>
+<tr>
+<td style="font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif; font-size: 13px; color: rgb(97, 101, 109);text-align:center ">
+<input id="username1" class="style6" maxlength="16" name="username1" type="text" value="" /></td>
+</tr>
+<tr><td>&nbsp;</td></tr>
+<tr>
+<td style="text-align:center">
+<input class="style8" type="submit" value="Check Usage" /></td>
+</tr>
+</table>
+</div>
+</td>
+</tr>
+<tr><td colspan="3" valign="top"></td></tr>
+</table>
+</form>
+</div>
+</div>
+</body>
+</html>
